7c87361ac6f0ff1c698120fe23c344ad6ef3994a
Henry Eklind <henry@karlek.io>
2022-02-01 02:07:07 +0100

    A20 is on!

--- src/protected_mode.asm
+++ src/protected_mode.asm
@@ -21,6 +21,36 @@ init_protected_mode:
 	mov ebp, 0x90000
 	mov esp, ebp
 
+	call is_A20_on
+	call is_A20_on
+	jmp $
+
+	ret
+
+; Check A20 line
+; Returns to caller if A20 gate is cleared.
+; Continues to A20_on if A20 line is set.
+; Written by Elad Ashkcenazi
+is_A20_on:
+	pushad
+
+	mov edi, 0x112345  ; Odd megabyte address.
+	mov esi, 0x012345  ; Even megabyte address.
+
+	mov [esi], esi     ; Making sure that both addresses contain different values.
+	mov [edi], edi     ; (if A20 line is cleared the two pointers would point to the address 0x012345 that would contain 0x112345 (edi))
+
+	cmpsd              ; Compare addresses to see if they're equivalent.
+	popad
+	jne A20_on         ; If not equivalent, A20 line is set.
+
+	mov eax, 0xcafe
+	jmp $
+	ret                ; If equivalent, the A20 line is cleared.
+
+A20_on:
+	mov eax, 0xdead
+	jmp $
 	ret
 
 %include "src/protected_mode_gdt.asm"
