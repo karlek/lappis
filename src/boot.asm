8e5783a0f1ed3da22bc4eae5e58a5b21ca5bec09
Henry Eklind <henry@karlek.io>
2022-02-05 23:54:10 +0100

    Setup VESA video mode for DMA framebuffer

--- src/boot.asm
+++ src/boot.asm
@@ -7,12 +7,27 @@ org 0x7c00
 KERNEL_OFFSET equ 0x1000
 
 start:
-	; Set video mode to 80x25 text, 16 foreground colors, 8 background colors
-	; https://web.archive.org/web/20121213064559/http://www.ousob.com/ng/asm/ng6fcde.php
-	mov ah, 0x0 ; Set video mode. 
-	mov al, 0x3 ; Video mode number.
+	; We need to get mode info in order to get the framebuffer address.
+	mov ax, 0x4f01        ; Get Mode Info.
+	mov cx, 0x011B        ; Mode number, 1280x1024, rgb, 24-bit (8:8:8)
+	mov di, 0xa000+0x400  ; Location of mode info.
 	int 0x10
-	
+
+	mov bx, 0x411b ; Linear framebuffer version of 1280x1024, rgb, 24-bit (8:8:8) (0x011b | 0x4000)
+	mov ax, 0x4f02 ; Set mode.
+	int 0x10
+
+	; In order for the firmware built into the system to optimize itself for
+	; running in Long Mode, AMD recommends that the OS notify the BIOS about
+	; the intended target environment that the OS will be running in: 32-bit
+	; mode, 64-bit mode, or a mixture of both modes. This can be done by
+	; calling the BIOS interrupt 15h from Real Mode with AX set to 0xEC00, and
+	; BL set to 1 for 32-bit Protected Mode, 2 for 64-bit Long Mode, or 3 if
+	; both modes will be used. 
+	mov ax, 0xec00
+	mov bx, 2
+	int 0x15
+
 	; Load our kernel
 	call .load_kernel
 
@@ -29,9 +44,6 @@ start:
 	call KERNEL_OFFSET
 
 .load_kernel:
-	mov bx, MSG_LOAD_KERNEL
-	call print
-
 	mov bx, KERNEL_OFFSET
 	mov dh, 47
 	mov dl, [BOOT_DRIVE]
@@ -45,7 +57,6 @@ start:
 
 ; Global variables
 BOOT_DRIVE: db 0
-MSG_LOAD_KERNEL: db "Loading kernel into memory.", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
