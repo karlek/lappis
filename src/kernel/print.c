a09fcb12406507fa593312c7d0debe49f04b9551
Henry Eklind <henry@karlek.io>
2022-02-16 23:39:49 +0100

    Remove dead kernel printing

--- src/kernel/print.c
+++ src/kernel/print.c
@@ -1,16 +1,3 @@
-// Parse newline and set cursor to next row.
-void kprint(char *string) {}
-
-void kprint_at(char *string, int col, int row) {}
-
-void write_string(int color, const char *string) {
-	volatile char *video = (volatile char*)VIDEO_ADDRESS;
-	while(*string != 0) {
-		*video++ = *string++;
-		*video++ = color;
-	}
-}
-
 void sprintf(unsigned char *out, unsigned char *format, ...) {
 	va_list args;
 	va_start(args, format);
