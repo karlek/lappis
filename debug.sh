d8f9084e9305a772733a5c92135fea45fad4f597
Henry Eklind <henry@karlek.io>
2022-03-02 01:06:35 +0100

    Fix serial listening

--- debug.sh
+++ debug.sh
@@ -2,12 +2,20 @@
 
 set -e
 
+cat /dev/null > /tmp/serial.log
+cat /dev/null > /tmp/serial.raw
+alacritty --class lapis-serial-log -e fish -c 'tail -f /tmp/serial.raw' &
+alacritty --class lapis-serial-log -e tail -f /tmp/serial.log &
+sleep 1
+
 # -S     Do not start CPU at startup (you must type 'c' in the monitor).
 alacritty -e \
 	qemu-system-x86_64 \
 	-S \
 	-gdb tcp::1234 \
-	-hda bin/fs.img \
+	-serial file:/tmp/serial.log \
+	-serial file:/tmp/serial.raw \
+	-drive media=disk,index=0,file=bin/fs.img,format=raw,if=ide \
 	-cdrom bin/kernel.iso &
 
 gdb \
--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ set -e
 
 cat /dev/null > /tmp/serial.log
 cat /dev/null > /tmp/serial.raw
+alacritty --class lapis-serial-log -e fish -c 'tail -f /tmp/serial.raw' &
 alacritty --class lapis-serial-log -e tail -f /tmp/serial.log &
-alacritty --class lapis-serial-log -e fish -c 'tail -f /tmp/serial.raw | xxd' &
 sleep 1
 
 qemu-system-x86_64 \
@@ -16,7 +16,7 @@ qemu-system-x86_64 \
 	-d int \
 	-serial file:/tmp/serial.log \
 	-serial file:/tmp/serial.raw \
-	-hda bin/fs.img \
+	-drive media=disk,index=0,file=bin/fs.img,format=raw,if=ide \
 	-cdrom bin/kernel.iso 
 
 ps ax | grep alacritty | grep lapis-serial-log | awk '{print $1}' | xargs kill
