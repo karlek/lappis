7f718072226b4fac225706663aa39a3968c9bb69
Henry Eklind <henry@karlek.io>
2022-03-12 17:56:25 +0100

    multiboot: Refactor to fix undefined c behavior
    Apparently, you are not allowed to define variables in case branch.

--- src/kernel/multiboot.c
+++ src/kernel/multiboot.c
@@ -72,53 +72,65 @@ void parse_multiboot_header(multiboot_info_t* boot_info) {
 		}
 
 		switch (tag->type) {
-		case MULTIBOOT_TAG_TYPE_CMDLINE:
-			multiboot_tag_string_t* cmdline = (multiboot_tag_string_t*)tag;
-			debug("\tCmdline: %s", cmdline->string);
-			break;
-		case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME:
-			multiboot_tag_string_t* boot_loader_name = (multiboot_tag_string_t*)tag;
-			debug("\tBoot loader name: %s", boot_loader_name->string);
-			break;
-		case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
-			multiboot_tag_basic_meminfo_t* meminfo = (multiboot_tag_basic_meminfo_t*)tag;
-			debug("\tMemory: %dKB/%dKB", meminfo->mem_lower, meminfo->mem_upper);
-			break;
-		case MULTIBOOT_TAG_TYPE_BOOTDEV:
-			multiboot_tag_bootdev_t* bootdev = (multiboot_tag_bootdev_t*)tag;
-			debug("\tBoot device: %d:%d", bootdev->biosdev, bootdev->slice);
-			break;
-		case MULTIBOOT_TAG_TYPE_MMAP:
-			multiboot_tag_mmap_t* mmap = (multiboot_tag_mmap_t*)tag;
-			debug("\tMemory map: %x", mmap->entry_size);
-			break;
-		case MULTIBOOT_TAG_TYPE_VBE:
-			multiboot_tag_vbe_t* vbe = (multiboot_tag_vbe_t*)tag;
-			debug("\tVBE: %d", vbe->vbe_mode);
-			break;
-		case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
-			multiboot_tag_framebuffer_t* framebuffer = (multiboot_tag_framebuffer_t*)tag;
-			debug("\tFramebuffer: %d x %d, %d bpp", framebuffer->common.framebuffer_width, framebuffer->common.framebuffer_height, framebuffer->common.framebuffer_bpp);
-			break;
-		case MULTIBOOT_TAG_TYPE_ELF_SECTIONS:
-			multiboot_tag_elf_sections_t* elf_sections = (multiboot_tag_elf_sections_t*)tag;
-			debug("\tELF sections: %x", elf_sections->num);
-			break;
-		case MULTIBOOT_TAG_TYPE_APM:
-			multiboot_tag_apm_t* apm = (multiboot_tag_apm_t*)tag;
-			debug("\tAPM info: %x", apm->version);
-			break;
-		case MULTIBOOT_TAG_TYPE_ACPI_OLD:
-			multiboot_tag_old_acpi_t* acpi = (multiboot_tag_old_acpi_t*)tag;
-			debug("\tACPI: %x (%d bytes)", acpi->rsdp.RsdtAddress, acpi->size);
-			break;
-		case MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR:
-			multiboot_tag_load_base_addr_t* base_addr = (multiboot_tag_load_base_addr_t*)tag;
-			debug("\tBase address: %x", base_addr->load_base_addr);
-			break;
-		default:
-			error("Unknown tag: %d", tag->type);
-			break;
+			case MULTIBOOT_TAG_TYPE_CMDLINE: {
+				multiboot_tag_string_t* cmdline = (multiboot_tag_string_t*)tag;
+				debug("\tCmdline: %s", cmdline->string);
+				break;
+			}
+			case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME: {
+				multiboot_tag_string_t* boot_loader_name = (multiboot_tag_string_t*)tag;
+				debug("\tBoot loader name: %s", boot_loader_name->string);
+				break;
+			}
+			case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO: {
+				multiboot_tag_basic_meminfo_t* meminfo = (multiboot_tag_basic_meminfo_t*)tag;
+				debug("\tMemory: %dKB/%dKB", meminfo->mem_lower, meminfo->mem_upper);
+				break;
+			}
+			case MULTIBOOT_TAG_TYPE_BOOTDEV: {
+				multiboot_tag_bootdev_t* bootdev = (multiboot_tag_bootdev_t*)tag;
+				debug("\tBoot device: %d:%d", bootdev->biosdev, bootdev->slice);
+				break;
+			}
+			case MULTIBOOT_TAG_TYPE_MMAP: {
+				multiboot_tag_mmap_t* mmap = (multiboot_tag_mmap_t*)tag;
+				debug("\tMemory map: %x", mmap->entry_size);
+				break;
+			}
+			case MULTIBOOT_TAG_TYPE_VBE: {
+				multiboot_tag_vbe_t* vbe = (multiboot_tag_vbe_t*)tag;
+				debug("\tVBE: %d", vbe->vbe_mode);
+				break;
+			}
+			case MULTIBOOT_TAG_TYPE_FRAMEBUFFER: {
+				multiboot_tag_framebuffer_t* framebuffer = (multiboot_tag_framebuffer_t*)tag;
+				debug("\tFramebuffer: %d x %d, %d bpp", framebuffer->common.framebuffer_width, framebuffer->common.framebuffer_height, framebuffer->common.framebuffer_bpp);
+				break;
+			}
+			case MULTIBOOT_TAG_TYPE_ELF_SECTIONS: {
+				multiboot_tag_elf_sections_t* elf_sections = (multiboot_tag_elf_sections_t*)tag;
+				debug("\tELF sections: %x", elf_sections->num);
+				break;
+			}
+			case MULTIBOOT_TAG_TYPE_APM: {
+				multiboot_tag_apm_t* apm = (multiboot_tag_apm_t*)tag;
+				debug("\tAPM info: %x", apm->version);
+				break;
+			}
+			case MULTIBOOT_TAG_TYPE_ACPI_OLD: {
+				multiboot_tag_old_acpi_t* acpi = (multiboot_tag_old_acpi_t*)tag;
+				debug("\tACPI: %x (%d bytes)", acpi->rsdp.RsdtAddress, acpi->size);
+				break;
+			}
+			case MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR: {
+				multiboot_tag_load_base_addr_t* base_addr = (multiboot_tag_load_base_addr_t*)tag;
+				debug("\tBase address: %x", base_addr->load_base_addr);
+				break;
+			}
+			default: {
+				error("Unknown tag: %d", tag->type);
+				break;
+			}
 		}
 
 		uint32_t next = tag->size % 8 == 0 ? (tag->size / 8) : (tag->size+8) / 8;
