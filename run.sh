c7dbeedfe9b77bcc190fff6e0717fd8cda5d7589
Henry Eklind <henry@karlek.io>
2022-03-12 17:55:47 +0100

    net: Add explicit netdev

--- run.sh
+++ run.sh
@@ -17,6 +17,7 @@ qemu-system-x86_64 \
 	-serial file:/tmp/serial.raw \
 	-drive media=disk,index=0,file=bin/zipfs.img,format=raw,if=ide \
 	-drive media=disk,index=1,file=bin/fat32.img,format=raw,if=ide \
+	-device e1000,netdev=n1,mac=52:54:00:12:34:56 \
 	-cdrom bin/kernel.iso 
 
 ps ax | grep alacritty | grep lapis-serial-log | awk '{print $1}' | xargs kill
