05b9fa59430551b5d14731cb2e15ebab3abe7523
Robin Eklind <rnd0x00@gmail.com>
2022-05-21 11:20:20 +0200

    boot: rewrite boot.asm in Zig

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,60 +1,8 @@
-; vim: set ft=nasm
-
 bits 16
-; org 0x800
-align 8
 
 MULTIBOOT_ORG: equ 0x100000
 
-MULTIBOOT_PAGE_ALIGN: equ 0x1
-MULTIBOOT_VIDEO_MODE: equ 0x4
-
-MULTIBOOT_HEADER_TAG_END:           equ 0
-MULTIBOOT_HEADER_TAG_OPTIONAL:      equ 1
-MULTIBOOT_HEADER_TAG_ADDRESS:       equ 2
-MULTIBOOT_HEADER_TAG_ENTRY_ADDRESS: equ 3
-MULTIBOOT_HEADER_TAG_FRAMEBUFFER:   equ 5
-
-MULTIBOOT_ARCHITECTURE_I386: equ 0
-
-MB_MAGIC: equ 0xE85250D6
-MB_FLAGS: equ MULTIBOOT_ARCHITECTURE_I386
-
-section .multiboot
-multiboot_header:
-	dd MB_MAGIC                               ; magic
-	dd MB_FLAGS                               ; flags
-	dd multiboot_header_end-multiboot_header  ; length
-	dd -(MB_MAGIC+MB_FLAGS + (multiboot_header_end-multiboot_header))                   ; checksum
-
-align 8
-framebuffer_tag_start:
-	dw 5
-	dw 1
-	dd 20
-	dd 1280
-	dd 1024
-	dd 32
-
-align 8
-; Also last tag.
-framebuffer_tag_end:
-	dw 0
-	dw 0
-	dd 8
-
-multiboot_header_end:
-
 extern main
-global multiboot_start
-section .text
-bits 32
-multiboot_start:
-	; Todo: Since we nuke the stack, we can't return here.
-	; So we jump to main from `init_long_mode`...
-	;
-	; Note: we will never return.
-	call init_long_mode
 
 ; We have to do the switch to long mode in our dynamically loaded kernel code,
 ; since we have too many bytes of data/code to fit before the magic boot
---
+++ src/boot/boot.zig
@@ -0,0 +1,47 @@
+const std = @import("std");
+
+const multiboot2 = @cImport(@cInclude("multiboot2.h"));
+
+const MultiMupp = struct {
+    hdr: multiboot2.multiboot_header,
+
+    // NOTE: multiboot tags must be 8 byte aligned.
+    frame_buffer_tag: multiboot2.multiboot_header_tag_framebuffer,
+    padding: [4]u8, // TODO: replace with align(8)
+    end_tag: multiboot2.multiboot_header_tag,
+};
+
+const MAGIC = @as(u32, multiboot2.MULTIBOOT2_HEADER_MAGIC);
+const ARCH = @as(u32, multiboot2.MULTIBOOT2_ARCHITECTURE_I386);
+const LENGTH = @as(u32, @sizeOf(MultiMupp));
+
+export var multiboot linksection(".multiboot") = MultiMupp{
+    .hdr = .{
+        .magic = MAGIC,
+        .architecture = ARCH,
+        .header_length = LENGTH,
+        .checksum = @bitCast(u32, -@bitCast(i32, (MAGIC + ARCH + LENGTH))), // gotta love bitcasts.
+    },
+    .frame_buffer_tag = .{
+        .type = multiboot2.MULTIBOOT_HEADER_TAG_FRAMEBUFFER,
+        .flags = multiboot2.MULTIBOOT_FRAMEBUFFER_TYPE_RGB,
+        .size = @sizeOf(multiboot2.multiboot_header_tag_framebuffer),
+        .width = 1280,
+        .height = 1024,
+        .depth = 32,
+    },
+    .padding = [4]u8{ 0, 0, 0, 0 }, // TODO: use align 8 instead.
+    .end_tag = .{
+        .type = multiboot2.MULTIBOOT_HEADER_TAG_END,
+        .flags = 0,
+        .size = @sizeOf(multiboot2.multiboot_header_tag),
+    },
+};
+
+extern fn init_long_mode() void;
+
+export fn multiboot_start() callconv(.Naked) noreturn {
+    init_long_mode();
+
+    while (true) {}
+}
