d3bb97885333c29afac035776b62820d21d5cae7
Robin Eklind <rnd0x00@gmail.com>
2024-04-26 22:46:56 +0200

    copy userland code into userland segment

--- src/kernel/heap.c
+++ src/kernel/heap.c
@@ -18,6 +18,13 @@ void * HEAP_END() {
 	return (void *)KERNEL_DATA_END_ADDR;
 }
 
+#define PAGE_SIZE 0x200000
+
+void * USERLAND_START() {
+	// TODO: ensure that kernel memory and heap does not overlap. include guard
+	// page.
+	return ((void *)USERLAND_ADDR) + PAGE_SIZE + 0x1000; // ignore guard page and give room for userland stack.
+}
 
 void init_heap() {
 	heap_start   = HEAP_START();
@@ -38,6 +45,16 @@ void* malloc(uint64_t size) {
 	}
 
 	heap_current += size;
+
+	return ptr;
+}
+
+void* userland_alloc(uint64_t addr, uint64_t size) {
+	void* ptr = (void *)addr;
+	// Zero out the memory.
+	for (uint64_t i = 0; i < size; i++) {
+		((uint8_t*)ptr)[i] = 0;
+	}
 
 	return ptr;
 }
