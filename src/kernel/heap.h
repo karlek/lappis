80ec2c0bae6f27cf7078f2338783b1a961ae5733
Robin Eklind <rnd0x00@gmail.com>
2024-04-26 13:21:15 +0200

    add TODO for adding guard page in between kernel memory and heap

--- src/kernel/heap.h
+++ src/kernel/heap.h
@@ -6,7 +6,7 @@
 #include "serial.h"
 
 // 59MiB heap
-#define HEAP_START 0x300000
+#define HEAP_START 0x300000 // TODO: ensure that kernel memory and heap does not overlap. include guard page
 #define HEAP_END   0x3e00000
 
 // Kernel stack:       0x4000000 - 0x4800000
