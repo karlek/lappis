f3d0d7edd20a3a0e9e3a10e17e199f2e05c04cd3
Henry Eklind <henry@karlek.io>
2024-05-09 01:03:02 +0200

    run: sync with debug.sh

--- run.sh
+++ run.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 cat /dev/null > /tmp/serial.log
 cat /dev/null > /tmp/serial.raw
-alacritty --class lappis-serial-raw -e fish -c 'tail -f /tmp/serial.raw' &
+alacritty --class lappis-serial-raw -e tail -f /tmp/serial.raw &
 alacritty --class lappis-serial-log -e tail -f /tmp/serial.log &
 sleep 1
 
 # swaymsg '[app_id="lapis-serial-log"] move window to workspace 4'
-qemu-system-x86_64 \
-	-no-reboot \
-	-no-shutdown \
-	-d int \
-	-m size=4G \
-	-serial file:/tmp/serial.log \
-	-serial file:/tmp/serial.raw \
-	-drive media=disk,index=0,file=bin/zipfs.img,format=raw,if=ide \
-	-monitor stdio \
-	-cdrom bin/kernel.iso
+kitty --class lappis-qemu \
+	qemu-system-x86_64 \
+		-cpu qemu64,+smep,+smap \
+		-no-reboot \
+		-no-shutdown \
+		-d int \
+		-m size=4G \
+		-serial file:/tmp/serial.log \
+		-serial file:/tmp/serial.raw \
+		-drive media=disk,index=0,file=bin/zipfs.img,format=raw,if=ide \
+		-monitor stdio \
+		-cdrom bin/kernel.iso
 
 ps ax | grep alacritty | grep lappis-serial-raw | awk '{print $1}' | xargs kill
 ps ax | grep alacritty | grep lappis-serial-log | awk '{print $1}' | xargs kill
+ps ax | grep kitty | grep lappis-qemu       | awk '{print $1}' | xargs kill
