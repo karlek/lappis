eda876188a715f947fbac0265e7d4c03f9384a0d
Henry Eklind <henry@karlek.io>
2024-05-09 22:56:30 +0200

    debug: fix indentation

--- debug.sh
+++ debug.sh
@@ -19,18 +19,18 @@ kitty --class lappis-serial-log -e tail -f /tmp/serial.log &
 
 kitty --class lappis-qemu \
 	qemu-system-x86_64 \
-	-cpu qemu64,+smep,+smap \
-	-no-reboot \
-	-no-shutdown \
-	-S \
-	-gdb tcp::1234 \
-	-d int \
-	-m size=4G \
-	-monitor stdio \
-	-serial file:/tmp/serial.log \
-	-serial file:/tmp/serial.raw \
-	-drive media=disk,index=0,file=bin/zipfs.img,format=raw,if=ide \
-	-cdrom bin/kernel.iso &
+		-cpu qemu64,+smep,+smap \
+		-no-reboot \
+		-no-shutdown \
+		-S \
+		-gdb tcp::1234 \
+		-d int \
+		-m size=4G \
+		-monitor stdio \
+		-serial file:/tmp/serial.log \
+		-serial file:/tmp/serial.raw \
+		-drive media=disk,index=0,file=bin/zipfs.img,format=raw,if=ide \
+		-cdrom bin/kernel.iso &
 
 sleep 1
 
